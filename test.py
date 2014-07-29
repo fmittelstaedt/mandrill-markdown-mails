7ebf5ec959f1654d004b83c7006889440f35e632
fmittelstaedt <frederique@frederique-mittelstaedt.com>
2014-07-29 15:53:04 +0100

    Create test.py
    Test file with example of config dict and sample call.

---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Mandrill Markdown Mails
+# Author: Frédérique Mittelstaedt 2014
+# License: MIT
+# Requires the python modules BeautifulSoup, markdown and mandrill
+
+import markdownmandrillmails
+
+# Example
+# John Doe <john.doe@invalid.com> sends a mail using his Mandrill template "mandrill-template" to Jane Doe<jane.doe@invalid.com>.
+# The html template is expected to contain the placeholder *|message_html|*, the text template is expected to contain the placeholder *|message_txt|*.
+
+dict = {
+	"sender": {
+		"email": 'john.doe@invalid.com',
+		"name": 'John Doe'
+	},
+	"recipients": [
+		{
+			"name": 'Jane Doe',
+			"email": 'jane.doe@invalid.com',
+			"type": 'to'
+		}
+	],
+	"subject": 'The subject',
+	"variables": [
+		"message": "The message content in *Markdown*!"
+	]
+}
+
+sendMarkdownMandrillMail("mandrill-template", dict)
