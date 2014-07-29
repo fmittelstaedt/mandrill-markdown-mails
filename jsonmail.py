a2385900c644e01a0531fe9c1f9fbe62f0c68582
fmittelstaedt <frederique@frederique-mittelstaedt.com>
2014-07-29 16:06:51 +0100

    Create jsonmail.py
    Sending e-mails with template name and email information passed as a JSON as command line parameters

---
+++ jsonmail.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Mandrill Markdown Mails
+# Author: Frédérique Mittelstaedt 2014
+# License: MIT
+# Requires the python modules BeautifulSoup, markdown and mandrill
+
+# This example also requires the modules sys and json
+
+import sys
+import json
+import markdownmandrillmails
+
+# Script for sending an email on Mandrill with the template and a JSON containing the information provided as a command line parameters
+# First parameter: template
+# Second parameter: JSON with mail information
+
+if (len(sys.argv) != 3):
+  print "Two variables need to be passed on to this script."
+  raise
+
+template = sys.argv[1]
+dict = json.loads(sys.argv[2])
+
+sendMarkdownMandrillMail(template, dict)
